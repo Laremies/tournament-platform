c36ed99e702dcea25fcaf807065500a87171bdd9
Tin Nguyen <nguyentritin211@gmail.com>
2024-11-28 18:24:44 +0200

    Update format and add test

--- jest.config.ts
+++ jest.config.ts
@@ -206,15 +206,21 @@
 // };
 
 // export default createJestConfig(config);
-import nextJest from 'next/jest.js'
+import nextJest from 'next/jest.js';
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
-})
+});
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  moduleNameMapper: { '^@/components/(.*)$': '<rootDir>/components/$1', '@app/(.*)$': '<rootDir>/app/$1', '^@lib/(.*)$': '<rootDir>/lib/$1', '^@utils/(.*)$': '<rootDir>/utils/$1', '^@/(.*)$': '<rootDir>/$1', },
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '@app/(.*)$': '<rootDir>/app/$1',
+    '^@lib/(.*)$': '<rootDir>/lib/$1',
+    '^@utils/(.*)$': '<rootDir>/utils/$1',
+    '^@/(.*)$': '<rootDir>/$1',
+  },
   testEnvironment: 'jest-environment-jsdom',
-}
-export default createJestConfig(customJestConfig)
+};
+export default createJestConfig(customJestConfig);
